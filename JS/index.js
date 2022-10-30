3f2b6e87f77f5d2ec325ad6d9009980543a807d7
sofiadv <sofia.dv@gmail.com>
2022-10-30 16:58:34 -0300

    consumo de google maps api

--- JS/index.js
+++ JS/index.js
@@ -26,3 +26,15 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+
+function mostrarMapa(){
+  var coordenadas = {lat:-34.6365824 ,lng:-58.7939206};
+  var map = new google.maps.Map(document.getElementById('map'),{
+    zoom: 10,
+    center: coordenadas
+  });
+  var marker = new google.maps.Marker({
+    position: coordenadas,
+    map: map
+  });
+}
