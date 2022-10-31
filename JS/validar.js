49c7f2e09f88fb5b141f6df38507507c2dc7bcc2
GracielaRad <114112778+GracielaRad@users.noreply.github.com>
2022-10-31 15:23:06 -0300

    validacion formato teléfono

--- JS/validar.js
+++ JS/validar.js
@@ -1,9 +1,10 @@
 function validar() {
-    var apellido, nombre, correo, teléfono, comentarios, expresion;
+    var apellido, nombre, correo, teléfono, valor, comentarios, expresion;
     apellido = document.getElementById("apellido").value;
     nombre = document.getElementById("nombre").value;
     correo = document.getElementById("correo").value;
-    teléfono = document.getElementById("teléfono").value;
+    teléfono = document.getElementById("teléfono").value; 
+    valor = /^\d{2}-\d{4}-\d{4}$/;
     comentarios = document.getElementById("comentarios").value;
     expresion = /\w+@\w+\.+[a-z]/;
 
@@ -29,15 +30,10 @@ function validar() {
         return false;
     }
 
-    else if (teléfono.length>30){
-         alert ("El teléfono es muy largo");
-         return false;
-    
-    }
-    else if (isNaN(teléfono)){
-        alert ("El teléfono ingresado no es válido")
+    else if (!valor.test(teléfono)){
+        alert ("Formato de teléfono inválido, ingresar según xx-xxxx-xxxx");
         return false;
-    } 
+    }
     else if (comentarios.length>100){
          alert ("El comentario es muy largo");
          return false;
